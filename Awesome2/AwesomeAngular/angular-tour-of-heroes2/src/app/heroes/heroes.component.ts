2941cd8841f685633798ed94e2957792536f0945
Farhanuddin <farhanuddinfu@hotmail.com>
2017-12-26 02:03:42 +0500

    2ndCommit

--- Awesome2/AwesomeAngular/angular-tour-of-heroes2/src/app/heroes/heroes.component.ts
+++ Awesome2/AwesomeAngular/angular-tour-of-heroes2/src/app/heroes/heroes.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+//Imporitng heroes object so we can use it as a data type in this program.
+import { Hero } from '../hero';
+
+//Importing mock hero data service
+import {HEROES} from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -12,4 +18,13 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
   }
 
+  //SelectedHero variale holder for storing clicked hero object details
+  selectedHero:Hero;
+
+  heroes = HEROES;
+
+  //onSelect Method on which Hero object datatype is being passed.
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
